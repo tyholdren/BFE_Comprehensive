89d05a15c3f16a10f282e5715c73087f98a54831
tyholdren <tsholdre@syr.edu>
2024-06-07 14:25:58 +0200

    throttle

---
+++ 004.throttle.js
@@ -0,0 +1,34 @@
+// This is a JavaScript coding problem from BFE.dev
+
+/**
+ * @param {(...args:any[]) => any} func
+ * @param {number} wait
+ * @returns {(...args:any[]) => any}
+ */
+function throttle(func, wait) {
+  let isThrottled = false;
+  let lastArgs = null;
+  let timeoutId = null;
+
+  return function (...args) {
+    if (!isThrottled) {
+      isThrottled = true;
+      func.apply(this, args);
+
+      const timeoutFn = () => {
+        timeoutId = setTimeout(() => {
+          isThrottled = false;
+          clearTimeout(timeoutId);
+          if (lastArgs) {
+            func.apply(this, lastArgs);
+            lastArgs = null;
+            timeoutFn();
+          }
+        }, wait);
+      };
+      timeoutFn();
+    } else {
+      lastArgs = args;
+    }
+  };
+}
