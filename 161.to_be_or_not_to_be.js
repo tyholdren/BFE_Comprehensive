57a738a86d417426acfe7265b84281ac3b88db0c
tyholdren <tsholdre@syr.edu>
2024-06-14 03:57:46 +0200

    to be or not to be

---
+++ 161.to_be_or_not_to_be.js
@@ -0,0 +1,33 @@
+/**
+ * interface Matcher {
+ *  toBe(data: any): void
+ * }
+ */
+/**
+ * @param {any} input
+ * @returns {Matcher & {not: Matcher}}
+ */
+function myExpect(input) {
+  function toBe(expected) {
+    const result = Object.is(expected, input);
+    if (!result) {
+      throw new Error('there was an error');
+    }
+    return result;
+  }
+
+  function notToBe(expected) {
+    const result = Object.is(expected, input);
+    if (result) {
+      throw new Error('there was an error');
+    }
+    return result;
+  }
+
+  return {
+    toBe,
+    not: {
+      toBe: notToBe,
+    },
+  };
+}
