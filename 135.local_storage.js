2af9a7466343f9f403714d557980051d9e022204
tyholdren <tsholdre@syr.edu>
2024-07-04 21:25:12 -0400

    local storage updates

--- 135.local_storage.js
+++ 135.local_storage.js
@@ -25,3 +25,50 @@ window.myLocalStorage = {
     window.localStorage.clear();
   },
 };
+
+export class LocalStorage {
+  constructor() {
+    this.store = new Map();
+  }
+
+  getItem(key) {
+    return this.store.has(key) ? this.store.get(key) : null;
+  }
+
+  setItem(key, value, maxAge) {
+    this.store.set(key, value);
+    if (maxAge === 0) {
+      this.removeItem(key);
+    } else {
+      setTimeout(() => {
+        this.store.delete(key);
+      }, maxAge);
+    }
+  }
+
+  removeItem(key) {
+    if (this.store.has(key)) {
+      this.store.delete(key);
+    } else {
+      return 'Key does not exist';
+    }
+  }
+
+  clear() {
+    this.store = new Map();
+  }
+}
+
+const myLocalStorage = new LocalStorage();
+
+myLocalStorage.setItem('name', 'tyler', 1000);
+console.log(myLocalStorage.getItem('name'));
+setTimeout(() => {
+  const value = myLocalStorage.getItem('name');
+  console.log({ value });
+}, 500);
+
+setTimeout(() => {
+  const value = myLocalStorage.getItem('name');
+  console.log({ value });
+}, 2000);
