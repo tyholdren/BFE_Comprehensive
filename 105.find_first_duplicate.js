34c3938df4875a8b80bd9e68d1143134a3b11eaf
tyholdren <tsholdre@syr.edu>
2025-01-06 14:35:40 +0200

    first duplicate different approach

--- 105.find_first_duplicate.js
+++ 105.find_first_duplicate.js
@@ -14,3 +14,30 @@ function firstDuplicate(str) {
   }
   return null;
 }
+
+/**
+ * https://bigfrontend.dev/problem/find-the-first-duplicate-character-in-a-string
+ * @param {string} str
+ * @return {string | null}
+ */
+function firstDuplicate(str) {
+  const map = new Map();
+
+  for (const el of str) {
+    if (!map.has(el)) {
+      map.set(el, 0);
+    }
+    map.set(el, map.get(el) + 1);
+  }
+
+  let firstDuplicate = null;
+
+  for (const char of str) {
+    if (map.get(char) === 2) {
+      firstDuplicate = char;
+      break;
+    }
+  }
+
+  return firstDuplicate;
+}
