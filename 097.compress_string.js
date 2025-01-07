e7cf2e78971013eef50c07d391e426d6dbecfb8e
tyholdren <tsholdre@syr.edu>
2025-01-07 15:38:17 +0200

    update compress string

--- 097.compress_string.js
+++ 097.compress_string.js
@@ -32,3 +32,64 @@ function compress(str) {
 
   return compressed;
 }
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+function _compress(str) {
+  if (str.length === 1) {
+    return str;
+  }
+
+  let stack = [];
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const curChar = str[i];
+    const prevChar = stack[stack.length - 1];
+
+    if (stack.length && curChar !== prevChar) {
+      const freq = stack.length === 1 ? '' : stack.length;
+      result += `${prevChar}${freq}`;
+      stack = [];
+
+      stack.push(curChar);
+    } else {
+      stack.push(curChar);
+    }
+  }
+
+  const freq = stack.length === 1 ? '' : stack.length;
+  const prevChar = stack[stack.length - 1];
+  result += `${prevChar}${freq}`;
+
+  return result;
+}
+
+function compress(str) {
+  if (str.length === 1) {
+    return str;
+  }
+
+  let prevChar = str[0];
+  let count = 1;
+  let result = '';
+
+  for (let i = 1; i < str.length; i += 1) {
+    const curChar = str[i];
+    if (curChar !== prevChar) {
+      const freq = count === 1 ? '' : count;
+      result += `${prevChar}${freq}`;
+      prevChar = curChar;
+      count = 1;
+    } else {
+      count += 1;
+    }
+  }
+
+  const freq = count === 1 ? '' : count;
+  result += `${prevChar}${freq}`;
+
+  return result;
+}
