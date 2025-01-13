8c7fa6697aebe14b2bf03eab62c349c8120f77a0
tyholdren <tsholdre@syr.edu>
2025-01-13 10:32:50 -0500

    throttle promises - optimized

--- 092.throttle_promises.js
+++ 092.throttle_promises.js
@@ -5,7 +5,7 @@
  * @param {number} max
  * @return {Promise}
  */
-async function throttlePromises(funcs, max) {
+async function _throttlePromises(funcs, max) {
   const results = [];
   let response;
 
@@ -19,4 +19,42 @@ async function throttlePromises(funcs, max) {
     }
   }
   return results;
+}
+
+/**
+ * @param {() => Promise<any>} func
+ * @param {number} max
+ * @return {Promise}
+ */
+function throttlePromises(funcs, max) {
+  const results = Array(funcs.length).fill(null);
+  let nextIndex = 0;
+  let resolved = 0;
+
+  return new Promise((resolve, reject) => {
+    async function process(index) {
+      nextIndex += 1; // INCREMENT NEXTINDEX IMMEDIATELY
+
+      try {
+        const curFn = funcs[index];
+        const curResult = await curFn();
+        results[index] = curResult;
+        resolved += 1;
+
+        if (resolved === funcs.length) {
+          resolve(results);
+          return;
+        } else if (nextIndex < funcs.length) {
+          // REMEMBER TO CHECK THIS BEFORE CALLING NEXTINDEX
+          process(nextIndex);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    }
+
+    for (let i = 0; i < Math.min(funcs.length, max); i += 1) {
+      process(i);
+    }
+  });
 }
