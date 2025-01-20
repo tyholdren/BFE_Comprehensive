77ab9bf45989be85c42e3f2f8d365965afbed7d1
tyholdren <tsholdre@syr.edu>
2025-01-19 23:04:35 -0500

    optimized unique chars

--- 087.longest_subString.js
+++ 087.longest_subString.js
@@ -3,7 +3,7 @@
  * @param {string} str
  * @return {string}
  */
-function longestUniqueSubstr(str) {
+function _longestUniqueSubstr(str) {
   let curChars = [];
   let curLength = 0;
   let maxLength = { length: 0, subString: '' };
@@ -26,4 +26,30 @@ function longestUniqueSubstr(str) {
   }
 
   return maxLength.subString;
+}
+
+/**
+ * https://bigfrontend.dev/problem/longest-substring-with-unique-characters
+ * @param {string} str
+ * @return {string}
+ */
+function longestUniqueSubstr(str) {
+  if (str.length === 0) return str;
+
+  let seen = new Set();
+  let maxChars = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (seen.has(str[i])) {
+      if (seen.size > maxChars.length) {
+        const word = [...seen].join('');
+        maxChars = word;
+        seen = new Set();
+      }
+    }
+
+    seen.add(str[i]);
+  }
+
+  return maxChars;
 }
